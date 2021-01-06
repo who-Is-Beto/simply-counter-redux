e2787bd2fdc8a1c2560f0e4363c7eb33f9ddea61
Beto Cortés <roberto.cortes13233@gmail.com>
2021-01-06 17:29:21 -0600

    A simple example in the console

--- src/index.js
+++ src/index.js
@@ -2,6 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import { createStore } from 'redux'
+
+//STORE => GLOBALIZED
+
+
+//ACTIONS (DESCRIBE)
+
+const increment = () => {
+  return {
+    type: 'INCREMENT',
+  }
+}
+
+const decremet = () => {
+  return {
+    type: 'DECREMENT',
+  }
+}
+
+//REDUCER
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+
+
+    default:
+      return state
+  }
+}
+
+//DISPLAY IT IN THE CONSOLE
+let store = createStore(counter)
+store.subscribe(() => console.log(store.getState()))
+//DISPATCH
+store.dispatch(increment())
+store.dispatch(decremet())
+store.dispatch(decremet())
 
 ReactDOM.render(
   <App />,
